2a46f1076f24cce275699e4887e1b132346781a6
yvonnewat <83101258+yvonnewat@users.noreply.github.com>
2022-01-11 14:48:33 +1300

    Create setup-script.sh

---
+++ Terraform/setup-script.sh
@@ -0,0 +1,47 @@
+#!bin/bash
+set +x
+/tmp/ddns-script $host_name $domain_name $(ec2metadata --public-ipv4) $ddns_password
+set -x
+
+# This delay to allow DNS propagation to take place
+sleep 3m
+
+# Create nginx proxy
+docker pull nginxproxy/nginx-proxy
+docker run --detach \
+--name nginx-proxy \
+--publish 80:80 \
+--publish 443:443 \
+--volume certs:/etc/nginx/certs \
+--volume vhost:/etc/nginx/vhost.d \
+--volume html:/usr/share/nginx/html \
+--volume /var/run/docker.sock:/tmp/docker.sock:ro \
+nginxproxy/nginx-proxy
+
+# Create nginx acme companion
+docker pull nginxproxy/acme-companion
+docker run --detach \
+--name nginx-proxy-acme \
+--volumes-from nginx-proxy \
+--volume /var/run/docker.sock:/var/run/docker.sock:ro \
+--volume acme:/etc/acme.sh \
+--env "DEFAULT_EMAIL=admin@ilikebubbletea.me" \
+nginxproxy/acme-companion
+    
+# Create etherpad container proxied with nginx
+docker pull etherpad/etherpad
+docker run -d \
+--name=etherpad \
+-e PUID=1000 \
+-e PGID=1000 \
+-e TZ=NZ \
+-p 443:443 \
+-v /path/to/appdata:/config \
+-v /path/to/data:/data \
+--restart unless-stopped \
+etherpad/etherpad
+    
+docker run -d --name nginx-proxy-acme \
+--env "VIRTUAL_HOST=$host_name.$domain_name" \
+--env "LETSENCRYPT_HOST=$host_name.$domain_name"  \
+--env "VIRTUAL_PORT=9001" --expose 9001 etherpad/etherpad
