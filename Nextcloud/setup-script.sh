90177fd7a48781a8b0296cfcf452c812ea9c9859
yvonnewat <83101258+yvonnewat@users.noreply.github.com>
2022-01-13 16:31:27 +1300

    Update setup-script.sh

--- Nextcloud/setup-script.sh
+++ Nextcloud/setup-script.sh
@@ -16,24 +16,24 @@ docker pull nginxproxy/acme-companion
 docker pull linuxserver/nextcloud
 
 # Run nginx-proxy
-docker run --detach \
- --name nginx-proxy \
- --publish 80:80 \
- --publish 443:443 \
- --volume certs:/etc/nginx/certs \
- --volume vhost:/etc/nginx/vhost.d \
- --volume html:/usr/share/nginx/html \
- --volume /var/run/docker.sock:/tmp/docker.sock:ro \
- nginxproxy/nginx-proxy
+#docker run --detach \
+# --name nginx-proxy \
+# --publish 80:80 \
+# --publish 443:443 \
+# --volume certs:/etc/nginx/certs \
+# --volume vhost:/etc/nginx/vhost.d \
+# --volume html:/usr/share/nginx/html \
+# --volume /var/run/docker.sock:/tmp/docker.sock:ro \
+# nginxproxy/nginx-proxy
     
 # Run acme-companion
-docker run --detach \
- --name nginx-proxy-acme \
- --volumes-from nginx-proxy \
- --volume /var/run/docker.sock:/var/run/docker.sock:ro \
- --volume acme:/etc/acme.sh \
- --env "DEFAULT_EMAIL=admin@$domain_name" \
- nginxproxy/acme-companion
+#docker run --detach \
+# --name nginx-proxy-acme \
+# --volumes-from nginx-proxy \
+# --volume /var/run/docker.sock:/var/run/docker.sock:ro \
+# --volume acme:/etc/acme.sh \
+# --env "DEFAULT_EMAIL=admin@$domain_name" \
+# nginxproxy/acme-companion
     
 # Run nextcloud container
 docker run -d \
@@ -41,6 +41,7 @@ docker run -d \
  -e PUID=1000 \
  -e PGID=1000 \
  -e TZ=NZ \
+ -p 443:443
  --env "VIRTUAL_HOST=$host_name.$domain_name" \
  --env "LETSENCRYPT_HOST=$host_name.$domain_name"  \
  --env "VIRTUAL_PORT=8000" \
