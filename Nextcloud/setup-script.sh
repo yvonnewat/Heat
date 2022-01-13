d15514b7ae6d1f63c1d739aecd05579004228df5
yvonnewat <83101258+yvonnewat@users.noreply.github.com>
2022-01-13 13:08:56 +1300

    Update setup-script.sh

--- Nextcloud/setup-script.sh
+++ Nextcloud/setup-script.sh
@@ -19,7 +19,6 @@ docker pull linuxserver/nextcloud
 docker run --detach \
  --name nginx-proxy \
  --publish 80:80 \
- --publish 443:443 \
  --volume certs:/etc/nginx/certs \
  --volume vhost:/etc/nginx/vhost.d \
  --volume html:/usr/share/nginx/html \
