59e54123ef6326b3ef42f2e644dfbf04fd62e5c4
yvonnewat <83101258+yvonnewat@users.noreply.github.com>
2022-01-27 16:44:10 +1300

    Create config.php

---
+++ Nextcloud/config.php
@@ -0,0 +1,32 @@
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'instanceid' => 'oc18kis8i0xe',
+  'passwordsalt' => '7Q2XP0zkKoCifJOsleV7OhQ5II8R+F',
+  'secret' => 'niIUMzeXMeadrnDxXUmQLHXd++d/J5H/jbatjEf8+o7+8fma',
+  'trusted_domains' => 
+  array (
+    0 => 'matcha.ilikebubbletea.me',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'sqlite3',
+  'version' => '23.0.0.10',
+  'overwrite.cli.url' => 'http://matcha.ilikebubbletea.me',
+  'installed' => true,
+);
